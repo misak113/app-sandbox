98f1f73ddb4188f449d981e2ca0af452f98fdbac
Michael Zabka <zabka.michael@gmail.com>
2015-10-11 12:11:53 +0200

    Add basic usage of default context for rect component

--- src/Layout/Main.tsx
+++ src/Layout/Main.tsx
@@ -1,10 +1,38 @@
 
 import * as React from 'react';
-import {Component} from 'react';
+import {Inject} from 'di-ts';
+import Component from '../React/Component';
+import DefaultContext from '../React/DefaultContext';
+import DateFactory from '../DateTime/DateFactory';
 
-export default class Main extends Component<{}, {}> {
-	
+@Inject
+export class MainContext {
+	constructor(
+		public dateFactory: DateFactory
+	) {}
+}
+
+@DefaultContext(MainContext)
+export default class Main extends Component<{}, { nowHumanized?: string }, MainContext> {
+
+	private nowInterval: NodeJS.Timer;
+
+	constructor(props, context) {
+		super(props, context);
+		this.state = {};
+	}
+
+	componentDidMount() {
+		this.nowInterval = setInterval(() => this.setState({
+			nowHumanized: this.context.dateFactory.now().toString()
+		}), 1000);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.nowInterval);
+	}
+
 	render() {
-		return <div/>;
+		return <div>{this.state.nowHumanized}</div>;
 	}
 }
