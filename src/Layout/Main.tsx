4c4072f2bcbd2bf4f7ca7a2e38b87d6d54e958aa
Michael Zabka <zabka.michael@gmail.com>
2015-10-13 02:32:58 +0200

    Add script of react frontend to Main layout
    * delete not used index.html which is rendered by Main layout

--- src/Layout/Main.tsx
+++ src/Layout/Main.tsx
@@ -7,8 +7,15 @@ export default class Main extends Component<{ children: any[] }, {}, {}> {
 	render() {
 		return (
 			<html>
-				<head></head>
-				<body>{this.props.children}</body>
+				<head>
+					<meta charSet="utf-8"/>
+					<title>App sandbox</title>
+					<link rel="stylesheet" media="all" href="/css/index.css"></link>
+				</head>
+				<body>
+					{this.props.children}
+					<script src="/front-bundle.js"></script>
+				</body>
 			</html>
 		);
 	}
