e94081f43ae9a00bf97e3a461e150de49d0ec7dc
Michael Zabka <zabka.michael@gmail.com>
2015-10-27 21:51:18 +0100

    Add next menu item & route
    * fix menu for sm responsible navigation toggled. Items are bellow brand, not in same line

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -5,11 +5,13 @@ import Homepage from '../Demo/Homepage';
 /* tslint:disable */
 var Route = require('react-router').Route;
 var IndexRoute = require('react-router').IndexRoute;
+var Route = require('react-router').Route;
 /* tslint:enable */
 
 var routes = (
-	<Route path='/' component={Main}>
+	<Route path="/" component={Main}>
 		<IndexRoute component={Homepage}/>
+		<Route path="/users" component={Homepage}/>
 	</Route>
 );
 
